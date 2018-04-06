2bf780035061fc1511cfe014b9d1ce15be3aa6e3
krdiamond <github email address>
2018-04-05 20:05:47 -0400

    added intro paragraph to first room

--- src/components/game_play/rooms/room1/room1.js
+++ src/components/game_play/rooms/room1/room1.js
@@ -10,6 +10,7 @@ class Room1 extends Component {
   state = {
     hoverGoToRoom2: false,
     hoverGoToRoom4: false,
+    openingStoryDisplayed: true,
   }
 
   handleGoToRoom = (roomNum) => {
@@ -42,6 +43,12 @@ class Room1 extends Component {
     }
   }
 
+  toggleShowStory = () => {
+    this.setState({
+      openingStoryDisplayed: !this.state.openingStoryDisplayed
+    })
+  }
+
 
   render() {
     return (
@@ -59,6 +66,13 @@ class Room1 extends Component {
           onMouseLeave={(e) => this.handlehoverLeaveGoTo(4)}>
           {this.state.hoverGoToRoom4? <div>GO TO JESSICA'S HOUSE</div> : null}
         </div>
+        <div id= "opening_story_closed"> </div>
+        <div id="show_story" onClick={this.toggleShowStory}>x</div>
+        {this.state.openingStoryDisplayed?
+          <div id="opening_story">
+             A girl named Kelly was found brutally murdered down by the beach two nights ago. Police found the body covered with fallen branches, rocks, sand and dirt.  The police also found numerous cigarette butts all over the area but were too covered with sand to test for DNA. The police have not been able to find any leads and the case is looking bleak. You are super nosy and have decided to snoop around to try and help. If you are able to figure out who the murderer is, please call 911 from any phone as soon as possible!!!!
+             <div id="hide_story" onClick={this.toggleShowStory}>x</div>
+          </div> : null}
       </div>
     );
   }
