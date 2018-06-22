c0158af87ddebb9e6a96f790bb276393f2e987bc
Kristina <kristina@verbalplusvisual.com>
2018-06-21 21:54:34 -0400

    done for tonight

--- src/components/game_play/start_game.js
+++ src/components/game_play/start_game.js
@@ -18,7 +18,7 @@ class StartGame extends Component {
         <div id="header">Murder In Miami</div>
         <div id="intro">
             <div id="opening_story">
-               A girl named Kelly was found brutally murdered by the beach two nights ago. Police found the body covered with fallen branches, rocks, sand and dirt.  They also found numerous cigarette butts all over the area but were too covered with sand to test for DNA. The police have not been able to find any leads and the case is looking bleak.  You are super nosy and have decided to snoop around to help solve the case.  You have come equiped with a duffle bag (on the right), to store any items that might help along the way. Point, click, and drag through Kelly's small Miami world to figure out what happened to her. If you are able to figure out who the murderer is and what the murder weapon was, please call the police from any phone as soon as possible!!!!
+               A girl named Kelly was found brutally murdered by the beach. Police have been combing the area for clues. The body was found covered with fallen branches, rocks, and sand. Cigarette butts were found all over the area but were too sandy to test for for DNA.  The police do not have any leads and the case is looking bleak.  You are nosy and have decided to snoop around. You are equiped with a duffle bag (on the right), to store any items that might help along the way. Explore Kelly's world to figure out what happened to her. If you think you know who the murderer is and what the murder weapon was, please call the police from any phone as soon as possible!!!!
             </div>
             <button id="start_button" type="button" onMouseUp={this.handleGoToRoom} >START GAME</button>
         </div>
